0f8e4e92bcda86d4695307c9f417c72632929a2c
satanas <wil.alejandro@gmail.com>
2014-01-23 13:13:09 -0300

    Benchmarking SFML

--- main.cpp
+++ main.cpp
@@ -1,28 +1,77 @@
+#include <vector>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <SFML/Graphics.hpp>
 
+#define MAX_FRAME_SAMPLES 30
+#define MAX_SPRITES 100
+
+int frames_count = 0;
+float fps = 0.0;
+
+float framesPerSecond(sf::Clock & clock) {
+    frames_count++;
+    if (frames_count >= MAX_FRAME_SAMPLES) {
+        fps = ((float)frames_count / (float)clock.getElapsedTime().asMilliseconds()) * 1000;
+        clock.restart();
+        frames_count = 0;
+    }
+    //if (clock.getElapsedTime().asMilliseconds() >= 1000) {
+    //    clock.restart();
+    //    fps = count;
+    //    count = 0;
+    //}
+    fflush(stdout);
+    return fps;
+}
+
 int main(int, char const**) {
+    int width = 1000;
+    int height = 600;
+    char buff[15];
+
+    srand(time(NULL));
+
+    sf::Clock clock;
+
     sf::ContextSettings settings;
     settings.antialiasingLevel = 8;
 
     // Create the main window
-    sf::RenderWindow window(sf::VideoMode(1000, 600), "My First SFML example", sf::Style::Default, settings);
-    //window.setVerticalSyncEnabled(true);
+    sf::RenderWindow window(sf::VideoMode(width, height), "My First SFML example", sf::Style::Default, settings);
     window.setFramerateLimit(60);
 
     sf::Font font;
     if (!font.loadFromFile("sansation.ttf")) {
-        // error
+        return EXIT_FAILURE;
     }
 
     sf::Text text;
     text.setFont(font);
-    text.setString("Hello World!");
+    text.setString("Hello Benchmark!");
     text.setCharacterSize(24);
+    int text_x = (width - (text.getCharacterSize() * 5)) / 2;
+    text.setPosition(text_x, 0);
     text.setColor(sf::Color::White);
 
-    sf::CircleShape shape(50);
-    shape.setFillColor(sf::Color(100, 250, 50));
-    shape.setPosition(50, 50);
+    sf::Text fps;
+    fps.setFont(font);
+    fps.setCharacterSize(20);
+    fps.setColor(sf::Color::White);
+
+    sf::Texture barrel;
+    if (!barrel.loadFromFile("barrel.png")) {
+        return EXIT_FAILURE;
+    }
+
+    std::vector<sf::Sprite> sprites(MAX_SPRITES);
+    for (int i=0; i<=MAX_SPRITES; i++) {
+        sprites[i].setTexture(barrel);
+        int x = rand() % width;
+        int y = rand() % height;
+        sprites[i].setPosition(x, y);
+    }
 
     while (window.isOpen()) {
         sf::Event event;
@@ -33,8 +82,19 @@ int main(int, char const**) {
 
         window.clear(sf::Color::Black);
 
+        sprintf(buff, "FPS: %3.2f", framesPerSecond(clock));
+        fps.setString(sf::String(buff));
+
+
+        for (int i=0; i<=MAX_SPRITES; i++) {
+            sf::Sprite s = sprites[i];
+            s.setScale(rand() % 2 + 1, 2);
+            s.setRotation((rand() % 720) - 360);
+            window.draw(s);
+        }
+
         window.draw(text);
-        window.draw(shape);
+        window.draw(fps);
 
         window.display();
     }
